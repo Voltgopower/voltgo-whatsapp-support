3ae419ae5b00e39313d4ac70e57c2da28819c3f5
Jeff Hu <your_email@example.com>
2026-03-08 21:53:24 -0700

    connect railway postgres

--- config/db.js
+++ config/db.js
@@ -1,8 +1,22 @@
 const { Pool } = require('pg');
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL not set');
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.on('connect', () => {
+  console.log('PostgreSQL connected');
+});
+
+pool.on('error', (err) => {
+  console.error('PostgreSQL error', err);
 });
 
 module.exports = pool;
